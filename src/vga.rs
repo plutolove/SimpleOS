a152c89c1e4ce99f42ad5b90805d65ef498ca278
plutolove <sa517255@mail.ustc.edu.cn>
2019-04-14 23:08:50 +0800

    implement vga

--- src/vga.rs
+++ src/vga.rs
@@ -20,3 +20,70 @@ pub enum Color {
     White = 15,
 }
 
+struct Char {
+    ch: u8,
+    color_code: u8,
+}
+
+const M: usize = 25;
+const N: usize = 80;
+
+
+#[repr(transparent)]
+pub struct Buffer {
+    chars: [[Char; N]; M],
+}
+
+pub struct Writer {
+    col: usize,
+    raw: usize,
+    color_code: u8,
+    buffer: &'static mut Buffer,
+}
+
+pub fn genColorCode(f: Color, b: Color) -> u8 {
+    return (((b as u8) << 4) | (f as u8));
+}
+
+impl Writer {
+
+    pub fn new(front: Color, back: Color) -> Self {
+        Self {
+            col: 0,
+            raw: 0,
+            color_code: genColorCode(Color::Green, Color::Black),
+            buffer: unsafe {&mut *(0xb8000 as *mut Buffer)},
+        }
+    }
+
+    fn new_line(&mut self) {
+        self.raw = (self.raw + 1) % M;
+        self.col = 0;
+    }
+
+    fn next(&mut self) {
+        self.col = (self.col + 1) % N;
+        if self.col == 0 {
+            self.raw = (self.raw + 1) % M;
+        }
+    }
+
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            _ => {
+                self.buffer.chars[self.raw][self.col] = Char{color_code: self.color_code, ch: byte};
+                self.next();
+            }
+        }
+    }
+
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                0x20...0x7e | b'\n' => self.write_byte(byte),
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+}
